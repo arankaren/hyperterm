c8203f8fddd25645d796e4b25b175a98b99f89a0
Jesús <heckyel@hyperbola.info>
2021-08-26 15:08:04 -0500

    Change domain to check Public IP

--- hyperterm/tools/network.sh
+++ hyperterm/tools/network.sh
@@ -4,5 +4,5 @@ function __network() {
     : "${MY_IP:=$(ip route show table local | awk -F "local" '{print $2}' | uniq)}"
 
     unset MY_ISP
-    : "${MY_ISP:=$(host myip.opendns.com resolver1.opendns.com | grep "myip.opendns.com has" | awk '{print $4}')}"
+    : "${MY_ISP:=$(host whoami.akamai.net. ns1-1.akamaitech.net. | grep "whoami.akamai.net has" | awk '{print $4}')}"
 }
